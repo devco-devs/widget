7acedcd0320af3727c522efd51d80b5b1057897d
Gabriel Prado <gaprados3@gmail.com>
2022-05-06 10:06:50 -0300

    Aula 05 - Adição a variável de ambiente PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-import { routes } from "./routes";
+import { routes } from './routes';
 
 const app = express();
 
@@ -9,4 +9,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Server is running..."));
+app.listen(process.env.PORT || 3333, () => console.log('Server is running...'));
